9c8772df8e61ab7500b0dfaea355fa59202ae202
Jamela <mejiajoyceai98@gmail.com>
2024-10-05 10:32:47 +0800

    Mama

--- index2.js
+++ index2.js
@@ -6,3 +6,9 @@ let y;
 x = 5;
 y = 6;
 let z = x + y;
+
+
+let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;    // Assign the sum of a and b to c
